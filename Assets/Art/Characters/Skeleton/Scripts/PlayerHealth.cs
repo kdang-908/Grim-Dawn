0dfc4f702981df0e17df10748072a3e30b437084
N-niche <nguyenthanhnhan21042004@gmail.com>
2025-12-10 13:53:58 +0700

    Add animator and new changes

---
+++ Assets/Art/Characters/Skeleton/Scripts/PlayerHealth.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health = 100;
+
+    public void TakeDamage(int dmg)
+    {
+        health -= dmg;
+        Debug.Log("Player bị đánh! Máu còn: " + health);
+
+        if (health <= 0)
+            Debug.Log("Player chết!");
+    }
+}
