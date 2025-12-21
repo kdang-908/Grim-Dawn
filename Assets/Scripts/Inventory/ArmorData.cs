a864dcbfc9419b3eacf1bc963336ad3e8ad6923b
nt <truongduong170111@gmail.com>
2025-12-21 20:52:32 +0700

    Update items and stats for item

---
+++ Assets/Scripts/Inventory/ArmorData.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+// Dòng này giúp tạo menu "Create -> Inventory -> Armor Data"
+[CreateAssetMenu(fileName = "New Armor Data", menuName = "Inventory/Armor Data")]
+public class ArmorData : ScriptableObject
+{
+    [Header("Thông tin cơ bản")]
+    public string id;
+    public string displayName;
+    public Sprite icon;
+    public GameObject prefab;
+
+    [Header("Chỉ số")]
+    public int defense; // Giáp thì có thủ (Def) thay vì công (Atk)
+
+    [Header("Căn chỉnh vị trí (Offset)")]
+    // Cái này để chỉnh nón cho vừa đầu, giống cái Offset của kiếm
+    public Vector3 localPos;
+    public Vector3 localEuler;
+    public Vector3 localScale = new Vector3(1, 1, 1);
+
+    [Header("Chỉ số cộng thêm")]
+    public int atkBonus;
+    public int defBonus;
+    public int hpBonus;
+
+}
